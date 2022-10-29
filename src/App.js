290a0a45274b0ee18019b316ed6cf6e217d6db3c
sauravarvind <sauravparameshwaran@gmail.com>
2022-10-29 09:54:07 +0530

    initial commit 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import Topbar from "./components/topbar/Topbar";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
+import Overallview from "./components/overallview/Overallview";
+import Listview from "./components/listview/Listview";
 
 
 function App() {
   return (
     <div className="App">
-     user management page
+     <Topbar/>
+     <Sidebar/>
+     <Header/>
+     <Overallview/>
+     <Listview/>
     </div>
   );
 }
